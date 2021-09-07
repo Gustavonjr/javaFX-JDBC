e85cb93f38d90e715173b32898fd5d7a3b5cdefc
Gustavo Nascimento <gustavonascimento_31@hotmail.com>
2021-09-07 09:27:02 -0300

    Consumer para Troca de telas

--- src/GUI/MainViewController.java
+++ src/GUI/MainViewController.java
@@ -3,6 +3,7 @@ package GUI;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import GUI.util.Alerts;
 import application.Main;
@@ -17,7 +18,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
@@ -25,67 +26,54 @@ public class MainViewController implements Initializable{
 	private MenuItem menuItemDepartment;
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/GUI/DepartmentList.fxml");
+		loadView("/GUI/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 
 	}
+
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/GUI/About.fxml");
-		
+		loadView("/GUI/About.fxml", x -> {
+		});
+
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
-		
+
 	}
-	//SYNCRONIZED PARA NÃO SER INTERROMPIDA NA HORA DA EXECUÇÃO
-	private synchronized void loadView(String absoluteName) {
+
+	// SYNCRONIZED PARA NÃO SER INTERROMPIDA NA HORA DA EXECUÇÃO
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-		VBox newVBox = loader.load();
-		
-		Scene mainScene = Main.getMainScene();
-		VBox mainVBox =(VBox)((ScrollPane)mainScene.getRoot()).getContent();
-		
-		//COLOCAR O ABOUT DENTRO DOS FILHOS(CHILDREN'S DA MAIN SCENE) PRESERVANDO O MENU
-		Node mainMenu = mainVBox.getChildren().get(0); //PEGANDO O MENU, PRIMEIRO FILHO DO VBOX(MAIN MENU)
-		mainVBox.getChildren().clear();//LIMPANDO TODOS OS FILHOS
-		mainVBox.getChildren().add(mainMenu);//PEGANDO O MENU E ADICIONANDO-O NOVAMENTE
-		mainVBox.getChildren().addAll(newVBox.getChildren());//ADICIONANDO OS NOVOS ITENS(ABOUT)
-		
-		}
-		catch(IOException e) {
-			Alerts.showAlert("IO Exeption", "Erro ao carregar a pagina" , e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private synchronized void loadView2(String absoluteName) {
-		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-		VBox newVBox = loader.load();
-		
-		Scene mainScene = Main.getMainScene();
-		VBox mainVBox =(VBox)((ScrollPane)mainScene.getRoot()).getContent();
-		
-		//COLOCAR O ABOUT DENTRO DOS FILHOS(CHILDREN'S DA MAIN SCENE) PRESERVANDO O MENU
-		Node mainMenu = mainVBox.getChildren().get(0); //PEGANDO O MENU, PRIMEIRO FILHO DO VBOX(MAIN MENU)
-		mainVBox.getChildren().clear();//LIMPANDO TODOS OS FILHOS
-		mainVBox.getChildren().add(mainMenu);//PEGANDO O MENU E ADICIONANDO-O NOVAMENTE
-		mainVBox.getChildren().addAll(newVBox.getChildren());//ADICIONANDO OS NOVOS ITENS(ABOUT)
-		
-		DepartmentListController controller = loader.getController();
-		controller.setDepartmentService(new DepartmentService());
-		controller.updateTableView();
-		}
-		catch(IOException e) {
-			Alerts.showAlert("IO Exeption", "Erro ao carregar a pagina" , e.getMessage(), AlertType.ERROR);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			// COLOCAR O ABOUT DENTRO DOS FILHOS(CHILDREN'S DA MAIN SCENE) PRESERVANDO O
+			// MENU
+			Node mainMenu = mainVBox.getChildren().get(0); // PEGANDO O MENU, PRIMEIRO FILHO DO VBOX(MAIN MENU)
+			mainVBox.getChildren().clear();// LIMPANDO TODOS OS FILHOS
+			mainVBox.getChildren().add(mainMenu);// PEGANDO O MENU E ADICIONANDO-O NOVAMENTE
+			mainVBox.getChildren().addAll(newVBox.getChildren());// ADICIONANDO OS NOVOS ITENS(ABOUT)
+			
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exeption", "Erro ao carregar a pagina", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
